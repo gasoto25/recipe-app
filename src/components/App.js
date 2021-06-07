10bb0cee86fcd2da6bb90b7aee216dd09d3afa0f
George Soto <sotogeorge08@gmail.com>
2021-06-06 23:19:44 -0700

    refactored sampleRecipes and moved to its own file

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import uuidv4 from 'uuid/dist/v4';
 import '../css/App.css';
 import RecipeEdit from './RecipeEdit';
 import RecipeList from './RecipeList';
+import { sampleRecipes } from '../sampleRecipes';
 
 export const RecipeContext = createContext();
 const LOCAL_STORAGE_KEY = 'RecipeApp.recipes';
@@ -78,45 +79,4 @@ function App() {
 	);
 }
 
-const sampleRecipes = [
-	{
-		id: 1,
-		name: 'Plain Chicken',
-		servings: 3,
-		cookTime: '1:45',
-		instructions:
-			'1. Put salt on chicken\n2.Put chicken in oven\n3. Eat Chicken',
-		ingredients: [
-			{
-				id: 1,
-				name: 'Chicken',
-				amount: '2 Pounds',
-			},
-			{
-				id: 2,
-				name: 'Salt',
-				amount: '1Tbs',
-			},
-		],
-	},
-	{
-		id: 2,
-		name: 'Plain Pork',
-		servings: 5,
-		cookTime: '0:45',
-		instructions: '1. Put paprika on pork\n2.Put pork in oven\n3. Eat Pork',
-		ingredients: [
-			{
-				id: 1,
-				name: 'Pork',
-				amount: '3 Pounds',
-			},
-			{
-				id: 2,
-				name: 'Paprika',
-				amount: '2Tbs',
-			},
-		],
-	},
-];
 export default App;
---
+++ src/sampleRecipes.js
@@ -0,0 +1,80 @@
+export const sampleRecipes = [
+	{
+		id: 1,
+		name: 'CREAMY HERB CHICKEN',
+		servings: 4,
+		cookTime: '0:30',
+		instructions: `1. Coat chicken breasts with the onion and garlic powders and herbs. Season generously with salt and pepper.\n2. Heat 1 tablespoon of oil a large pan or skillet over medium-high heat and cook chicken breasts until opaque and no longer pink inside (about 5 minutes each side, depending on thickness). Transfer to a plate; set aside.\n3. To the same pan or skillet, heat another 2 teaspoons of olive oil and sauté garlic, with parsley, thyme and rosemary, for about 1 minute, or until fragrant.\n`,
+		ingredients: [
+			{
+				id: 1,
+				name: 'Chicken Breast',
+				amount: '4',
+			},
+			{
+				id: 2,
+				name: 'Garlic Powder',
+				amount: '2 tsp',
+			},
+			{
+				id: 3,
+				name: 'Onion Powder',
+				amount: '2 tsp',
+			},
+			{
+				id: 4,
+				name: 'Dried Thyme',
+				amount: '1/2 tsp',
+			},
+			{
+				id: 5,
+				name: 'Dried Rosemary',
+				amount: '1/2 tsp',
+			},
+		],
+	},
+	{
+		id: 2,
+		name: 'Lemon Butter Fish',
+		servings: 4,
+		cookTime: '0:19',
+		instructions: `1. Use paper towels to thoroughly pat-dry excess moisture from fish fillets – this step is crucial for fish to brown nicely in pan. Set aside.\n2. In a bowl, combine melted butter, lemon juice and zest, and 1/2 tsp kosher salt. Stir to combine well. Taste and add a bit more kosher salt, if desired.\n3. In a separate bowl, combine the remaining 1/2 tsp kosher salt, paprika, garlic powder, onion powder, and black pepper. Evenly press spice mixture onto all sides of fish fillets.\n4. In a large, heavy pan over medium high heat, heat up the olive oil until hot. Once your oil is sizzling,  Cook 2 fish fillets at a time to avoid overcrowding (allows for even browning.) Cook each side just until fish becomes opaque, feels somewhat firm in the center, and is browned – lightly drizzle some of the lemon butter sauce as you cook, reserving the rest for serving. Take care not to over-cook, as that will result in a tougher texture. Season with extra kosher salt and freshly ground black pepper to taste.\n`,
+		ingredients: [
+			{
+				id: 1,
+				name: 'Halibut Fillets',
+				amount: '4 Pieces',
+			},
+			{
+				id: 2,
+				name: 'Butter',
+				amount: '3Tbs',
+			},
+			{
+				id: 3,
+				name: 'Lemon',
+				amount: '1 medium sized',
+			},
+			{
+				id: 4,
+				name: 'Salt',
+				amount: '1/2Tbs',
+			},
+			{
+				id: 5,
+				name: 'Paprika',
+				amount: '1Tsp',
+			},
+			{
+				id: 6,
+				name: 'Garlic Powder',
+				amount: '1Tsp',
+			},
+			{
+				id: 7,
+				name: 'Olive Oil',
+				amount: '3Tbs',
+			},
+		],
+	},
+];
